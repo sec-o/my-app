d614b97c0989f8348c63af3995e19d11cf949baa
sec-o <157708270+sec-o@users.noreply.github.com>
2024-01-28 15:50:29 -0300

    Add product-details site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProductFinderAppComponent } from './product-finder-app/product-finder-a
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ChatbotAppComponent } from './chatbot-app/chatbot-app.component';
 import { QrCodeGeneratorComponent } from './qr-code-generator/qr-code-generator.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'product-finder-app', component: ProductFinderAppComponent },
   { path: 'chatbot-app', component: ChatbotAppComponent },
   { path: 'qr-code-generator-app', component: QrCodeGeneratorComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
--- src/app/mock-data.service.ts
+++ src/app/mock-data.service.ts
@@ -16,37 +16,67 @@ export class MockDataService {
       name: 'Apple iPhone 18',
       rating: 5,
       description: 'Put your product description here. This is just a sample.',
+      fullDescription:
+        'Brand new product. Comes with lots of extras and a stylish package. If necessary, add more product description here. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example.',
       price: 1000,
+      condition: 'new',
+      inStock: true,
+      deliveryTime: 1,
     },
     {
       name: 'Samsung Galaxy S35',
       rating: 4,
       description: 'Put your product description here. This is just a sample.',
+      fullDescription:
+        'Brand new product. Comes with lots of extras and a stylish package. If necessary, add more product description here. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example.',
       price: 900,
+      condition: 'new',
+      inStock: true,
+      deliveryTime: 1,
     },
     {
       name: 'Sony Xperia 9',
       rating: 3.5,
       description: 'Put your product description here. This is just a sample.',
+      fullDescription:
+        'Brand new product. Comes with lots of extras and a stylish package. If necessary, add more product description here. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example.',
       price: 800,
+      condition: 'new',
+      inStock: true,
+      deliveryTime: 3,
     },
     {
       name: 'LG G 16',
       rating: 3,
       description: 'Put your product description here. This is just a sample.',
+      fullDescription:
+        'Brand new product. Comes with lots of extras and a stylish package. If necessary, add more product description here. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example.',
       price: 500,
+      condition: 'new',
+      inStock: true,
+      deliveryTime: 3,
     },
     {
       name: 'Nokia 14',
       rating: 3,
       description: 'Put your product description here. This is just a sample.',
+      fullDescription:
+        'Brand new product. Comes with lots of extras and a stylish package. If necessary, add more product description here. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example.',
       price: 300,
+      condition: 'new',
+      inStock: true,
+      deliveryTime: 2,
     },
     {
       name: 'Motorola Razr',
       rating: 4,
       description: 'Put your product description here. This is just a sample.',
+      fullDescription:
+        'Brand new product. Comes with lots of extras and a stylish package. If necessary, add more product description here. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example. This is only an example.',
       price: 450,
+      condition: 'new',
+      inStock: true,
+      deliveryTime: 2,
     },
     // Add more products as needed
   ];
