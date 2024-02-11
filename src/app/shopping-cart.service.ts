ca1b73b1b0ee87f778e6398673d61f7d56517312
sec-o <157708270+sec-o@users.noreply.github.com>
2024-02-11 16:05:18 -0300

    Added following new functions
    - "Add to Cart" button
    - Cart displays all products in a list
    - Notification pop-up when a product is added

---
+++ src/app/shopping-cart.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Product } from './models/product.model';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShoppingCartService {
+  private cachedProducts: Product[] = [];
+  private productsSubject = new BehaviorSubject<Product[]>([]);
+
+  constructor() {}
+
+  getProducts(): Product[] {
+    return this.cachedProducts;
+  }
+
+  addToCart(product: Product, quantity: number): void {
+    // Initialize undefined property "quantityInShoppingCart"
+    if (!product.quantityInShoppingCart)
+      product.quantityInShoppingCart = quantity;
+
+    if (this.cachedProducts.find((product1) => product1.id === product.id)) {
+      const productIndex = this.cachedProducts.findIndex(
+        (product1) => product1.id === product.id
+      );
+      // Update "quantityInShoppingCart" with +1 if product already in shopping cart
+      const quantity1 =
+        this.cachedProducts[productIndex].quantityInShoppingCart;
+      if (quantity1)
+        this.cachedProducts[productIndex].quantityInShoppingCart =
+          quantity1 + quantity;
+    } else this.cachedProducts.push(product);
+    this.productsSubject.next(this.cachedProducts);
+  }
+
+  clearCart(): void {
+    this.cachedProducts = [];
+    this.productsSubject.next(this.cachedProducts);
+  }
+}
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css'],
 })
-export class ShoppingCartComponent {}
+export class ShoppingCartComponent {
+  products: Product[] = [];
+  productQuantityInCart?: number;
+
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  ngOnInit(): void {
+    this.products = this.shoppingCartService.getProducts();
+  }
+}
