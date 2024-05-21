b6281e04326771cfb4ea4846a9ab7c6eaa631050
sec-o <157708270+sec-o@users.noreply.github.com>
2024-05-21 18:59:10 +0200

    Add fallback routing for github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirectTo = urlParams.get('redirect');
+    if (redirectTo) {
+      this.router.navigateByUrl(decodeURIComponent(redirectTo));
+    }
+  }
 }
